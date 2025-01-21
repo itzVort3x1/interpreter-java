01f312fa9bdd18135ab8211d465ec06280f97ab9
itzVort3x1 <saikaustubh10@gmail.com>
2025-01-21 22:07:46 +0000

    codecrafters submit [skip ci]

--- src/main/java/Scanner.java
+++ src/main/java/Scanner.java
@@ -120,6 +120,8 @@ class Scanner {
     }
 
     private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
         String text = source.substring(start, current);
         TokenType type = keywords.get(text);
         if (type == null) type = TokenType.IDENTIFIER;
